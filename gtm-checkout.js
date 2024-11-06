44e3daa25920e078810975ab55cf744be2ae6a3d
SuperDuperGuy <nicolas.grissi@gmail.com>
2024-11-06 17:23:25 +0100

    test2

--- gtm-checkout.js
+++ gtm-checkout.js
@@ -1,6 +1,7 @@
 function subscribeOnBodlEvents() {
     // log to verify that the function is executing
     console.log('run subscribeOnBodlEvents()');
+    console.log('test')
 
     if (
       // window is falsy
